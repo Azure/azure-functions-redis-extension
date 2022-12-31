6de1df04d03093ae97d76127233be1155b6d29e1
Maran Palaniappan <mapalan@microsoft.com>
2022-12-30 19:06:02 -0800

    add integration tests for bindings

--- test/Integration/IntegrationTestFunctions.cs
+++ test/Integration/IntegrationTestFunctions.cs
@@ -1,5 +1,6 @@
-using Microsoft.Extensions.Logging;
 using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
 {
@@ -10,67 +11,97 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
         public const string keyspaceKey = "testKey";
         public const string keyeventEvent = "set";
         public const string all = "*";
+        public const string bindingKey = "bindingKey";
+        public const string bindingValue = "bindingValue";
 
         [FunctionName(nameof(PubSubTrigger_SingleChannel))]
         public static void PubSubTrigger_SingleChannel(
-            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel)]
-            RedisMessageModel result, ILogger logger)
+            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel)] RedisMessageModel result,
+            ILogger logger)
         {
             logger.LogInformation(JsonSerializer.Serialize(result));
         }
 
         [FunctionName(nameof(PubSubTrigger_MultipleChannels))]
         public static void PubSubTrigger_MultipleChannels(
-        [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel + all)]
-            RedisMessageModel result, ILogger logger)
+        [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel + all)] RedisMessageModel result,
+        ILogger logger)
         {
             logger.LogInformation(JsonSerializer.Serialize(result));
         }
 
         [FunctionName(nameof(PubSubTrigger_AllChannels))]
         public static void PubSubTrigger_AllChannels(
-        [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = all)]
-            RedisMessageModel result, ILogger logger)
+        [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = all)] RedisMessageModel result,
+        ILogger logger)
         {
             logger.LogInformation(JsonSerializer.Serialize(result));
         }
 
         [FunctionName(nameof(KeySpaceTrigger_SingleKey))]
         public static void KeySpaceTrigger_SingleKey(
-            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = keyspaceKey)]
-            RedisMessageModel result, ILogger logger)
+            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = keyspaceKey)] RedisMessageModel result,
+            ILogger logger)
         {
             logger.LogInformation(JsonSerializer.Serialize(result));
         }
 
         [FunctionName(nameof(KeySpaceTrigger_MultipleKeys))]
         public static void KeySpaceTrigger_MultipleKeys(
-        [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = keyspaceKey + all)]
-            RedisMessageModel result, ILogger logger)
+        [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = keyspaceKey + all)] RedisMessageModel result,
+        ILogger logger)
         {
             logger.LogInformation(JsonSerializer.Serialize(result));
         }
 
         [FunctionName(nameof(KeySpaceTrigger_AllKeys))]
         public static void KeySpaceTrigger_AllKeys(
-        [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = all)]
-            RedisMessageModel result, ILogger logger)
+        [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = all)] RedisMessageModel result,
+        ILogger logger)
         {
             logger.LogInformation(JsonSerializer.Serialize(result));
         }
 
         [FunctionName(nameof(KeyEventTrigger_SingleEvent))]
         public static void KeyEventTrigger_SingleEvent(
-            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeyEvent, Trigger = keyeventEvent)]
-            RedisMessageModel result, ILogger logger)
+            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeyEvent, Trigger = keyeventEvent)] RedisMessageModel result,
+            ILogger logger)
         {
             logger.LogInformation(JsonSerializer.Serialize(result));
         }
 
         [FunctionName(nameof(KeyEventTrigger_AllEvents))]
         public static void KeyEventTrigger_AllEvents(
-            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeyEvent, Trigger = all)]
-            RedisMessageModel result, ILogger logger)
+            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeyEvent, Trigger = all)] RedisMessageModel result,
+            ILogger logger)
+        {
+            logger.LogInformation(JsonSerializer.Serialize(result));
+        }
+
+        [FunctionName(nameof(ConnectionBinding))]
+        public static void ConnectionBinding(
+            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel)] RedisMessageModel _,
+            [RedisConnection(ConnectionString = connectionString)] IConnectionMultiplexer multiplexer,
+            ILogger logger)
+        {
+            multiplexer.GetDatabase().StringSet(bindingKey, bindingValue);
+        }
+
+        [FunctionName(nameof(CommandBinding))]
+        public static void CommandBinding(
+            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel)] RedisMessageModel _,
+            [RedisCommand(ConnectionString = connectionString, RedisCommand = "get", Arguments = bindingKey)] RedisResult result,
+            ILogger logger)
+        {
+            logger.LogInformation(JsonSerializer.Serialize(result));
+        }
+
+
+        [FunctionName(nameof(ScriptBinding))]
+        public static void ScriptBinding(
+            [RedisTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel)] RedisMessageModel _,
+            [RedisScript(ConnectionString = connectionString)] RedisResult result,
+            ILogger logger)
         {
             logger.LogInformation(JsonSerializer.Serialize(result));
         }
---
+++ test/Integration/RedisBindingsTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using StackExchange.Redis;
+using System.Diagnostics;
+using Xunit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
+{
+    public class RedisBindingsTests
+    {
+        [Fact]
+        public void RedisConnection_ReturnsActiveMultiplexer()
+        {
+            bool success = false;
+            string bindingValue = null;
+            string functionName = nameof(IntegrationTestFunctions.ConnectionBinding);
+            Dictionary<string, int> counts = new Dictionary<string, int>
+            {
+                { $"Executed '{functionName}' (Succeeded", 1 },
+            };
+
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(IntegrationTestFunctions.connectionString))
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName))
+            {
+                TaskCompletionSource<bool> functionCompleted = new TaskCompletionSource<bool>();
+                functionsProcess.OutputDataReceived += IntegrationTestHelpers.CounterHandlerCreator(counts, functionCompleted);
+
+                multiplexer.GetDatabase().KeyDelete(IntegrationTestFunctions.bindingKey);
+                ISubscriber subscriber = multiplexer.GetSubscriber();
+                subscriber.Publish(IntegrationTestFunctions.pubsubChannel, "start");
+
+                success = functionCompleted.Task.Wait(TimeSpan.FromSeconds(1));
+                functionsProcess.Kill();
+
+                bindingValue = multiplexer.GetDatabase().StringGet(IntegrationTestFunctions.bindingKey);
+                multiplexer.Close();
+            };
+            Assert.Equal(IntegrationTestFunctions.bindingValue, bindingValue);
+            Assert.True(success, JsonSerializer.Serialize(counts));
+        }
+
+        [Fact]
+        public void RedisCommand_ReturnsCorrectValue()
+        {
+            bool success = false;
+            string functionName = nameof(IntegrationTestFunctions.CommandBinding);
+            Dictionary<string, int> counts = new Dictionary<string, int>
+            {
+                { $"Executed '{functionName}' (Succeeded", 1 },
+                { IntegrationTestFunctions.bindingValue + "1", 1 }
+            };
+
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(IntegrationTestFunctions.connectionString))
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName))
+            {
+                TaskCompletionSource<bool> functionCompleted = new TaskCompletionSource<bool>();
+                functionsProcess.OutputDataReceived += IntegrationTestHelpers.CounterHandlerCreator(counts, functionCompleted);
+
+                multiplexer.GetDatabase().StringSet(IntegrationTestFunctions.bindingKey, IntegrationTestFunctions.bindingValue + "1");
+                ISubscriber subscriber = multiplexer.GetSubscriber();
+                subscriber.Publish(IntegrationTestFunctions.pubsubChannel, "start");
+
+                success = functionCompleted.Task.Wait(TimeSpan.FromSeconds(1));
+                functionsProcess.Kill();
+                multiplexer.Close();
+            };
+            Assert.True(success, JsonSerializer.Serialize(counts));
+        }
+
+        [Fact]
+        public void RedisScript_ReturnsCorrectValue()
+        {
+            bool success = false;
+            string functionName = nameof(IntegrationTestFunctions.ScriptBinding);
+            Dictionary<string, int> counts = new Dictionary<string, int>
+            {
+                { $"Executed '{functionName}' (Succeeded", 1 },
+                { IntegrationTestFunctions.bindingValue + "2", 1 }
+            };
+
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(IntegrationTestFunctions.connectionString))
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName))
+            {
+                TaskCompletionSource<bool> functionCompleted = new TaskCompletionSource<bool>();
+                functionsProcess.OutputDataReceived += IntegrationTestHelpers.CounterHandlerCreator(counts, functionCompleted);
+
+                multiplexer.GetDatabase().StringSet(IntegrationTestFunctions.bindingKey, IntegrationTestFunctions.bindingValue + "2");
+                ISubscriber subscriber = multiplexer.GetSubscriber();
+                subscriber.Publish(IntegrationTestFunctions.pubsubChannel, "start");
+
+                success = functionCompleted.Task.Wait(TimeSpan.FromSeconds(1));
+                functionsProcess.Kill();
+                multiplexer.Close();
+            };
+            Assert.True(success, JsonSerializer.Serialize(counts));
+        }
+    }
+}
