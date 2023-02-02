d5bf662e4d2a81c715207061987fea2a2a420000
Maran Palaniappan <mapalan@microsoft.com>
2023-02-02 15:36:53 -0800

    rename variables

--- test/Integration/IntegrationTestFunctions.cs
+++ test/Integration/IntegrationTestFunctions.cs
@@ -13,66 +13,66 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
 
         [FunctionName(nameof(PubSubTrigger_SingleChannel))]
         public static void PubSubTrigger_SingleChannel(
-            [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel)]
-            RedisMessageModel result, ILogger logger)
+            [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel)] RedisMessageModel model,
+            ILogger logger)
         {
-            logger.LogInformation(JsonSerializer.Serialize(result));
+            logger.LogInformation(JsonSerializer.Serialize(model));
         }
 
         [FunctionName(nameof(PubSubTrigger_MultipleChannels))]
         public static void PubSubTrigger_MultipleChannels(
-        [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel + all)]
-            RedisMessageModel result, ILogger logger)
+        [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = pubsubChannel + all)] RedisMessageModel model,
+        ILogger logger)
         {
-            logger.LogInformation(JsonSerializer.Serialize(result));
+            logger.LogInformation(JsonSerializer.Serialize(model));
         }
 
         [FunctionName(nameof(PubSubTrigger_AllChannels))]
         public static void PubSubTrigger_AllChannels(
-        [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = all)]
-            RedisMessageModel result, ILogger logger)
+        [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.PubSub, Trigger = all)] RedisMessageModel model,
+        ILogger logger)
         {
-            logger.LogInformation(JsonSerializer.Serialize(result));
+            logger.LogInformation(JsonSerializer.Serialize(model));
         }
 
         [FunctionName(nameof(KeySpaceTrigger_SingleKey))]
         public static void KeySpaceTrigger_SingleKey(
-            [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = keyspaceKey)]
-            RedisMessageModel result, ILogger logger)
+            [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = keyspaceKey)] RedisMessageModel model,
+            ILogger logger)
         {
-            logger.LogInformation(JsonSerializer.Serialize(result));
+            logger.LogInformation(JsonSerializer.Serialize(model));
         }
 
         [FunctionName(nameof(KeySpaceTrigger_MultipleKeys))]
         public static void KeySpaceTrigger_MultipleKeys(
-        [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = keyspaceKey + all)]
-            RedisMessageModel result, ILogger logger)
+        [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = keyspaceKey + all)] RedisMessageModel model,
+        ILogger logger)
         {
-            logger.LogInformation(JsonSerializer.Serialize(result));
+            logger.LogInformation(JsonSerializer.Serialize(model));
         }
 
         [FunctionName(nameof(KeySpaceTrigger_AllKeys))]
         public static void KeySpaceTrigger_AllKeys(
-        [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = all)]
-            RedisMessageModel result, ILogger logger)
+        [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeySpace, Trigger = all)] RedisMessageModel model,
+        ILogger logger)
         {
-            logger.LogInformation(JsonSerializer.Serialize(result));
+            logger.LogInformation(JsonSerializer.Serialize(model));
         }
 
         [FunctionName(nameof(KeyEventTrigger_SingleEvent))]
         public static void KeyEventTrigger_SingleEvent(
-            [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeyEvent, Trigger = keyeventEvent)]
-            RedisMessageModel result, ILogger logger)
+            [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeyEvent, Trigger = keyeventEvent)] RedisMessageModel model,
+            ILogger logger)
         {
-            logger.LogInformation(JsonSerializer.Serialize(result));
+            logger.LogInformation(JsonSerializer.Serialize(model));
         }
 
         [FunctionName(nameof(KeyEventTrigger_AllEvents))]
         public static void KeyEventTrigger_AllEvents(
-            [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeyEvent, Trigger = all)]
-            RedisMessageModel result, ILogger logger)
+            [RedisPubSubTrigger(ConnectionString = connectionString, TriggerType = RedisTriggerType.KeyEvent, Trigger = all)] RedisMessageModel model,
+            ILogger logger)
         {
-            logger.LogInformation(JsonSerializer.Serialize(result));
+            logger.LogInformation(JsonSerializer.Serialize(model));
         }
     }
 }
