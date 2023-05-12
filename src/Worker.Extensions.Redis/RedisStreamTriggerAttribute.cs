a750f3c61e67dd5f5f7050c66b405428815e3e0f
Maran Palaniappan <mapalan@microsoft.com>
2023-05-12 10:11:25 -0700

    dotnet-isolated Worker.Extensions.Redis package (#62)

---
+++ src/Worker.Extensions.Redis/RedisStreamTriggerAttribute.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.Redis
+{
+    /// <summary>
+    /// Redis streams trigger binding attributes.
+    /// </summary>
+    public class RedisStreamTriggerAttribute : TriggerBindingAttribute
+    {
+        /// <summary>
+        /// Initializes a new <see cref="RedisStreamTriggerAttribute"/>.
+        /// </summary>
+        /// <param name="connectionStringSetting">Redis connection string setting.</param>
+        /// <param name="key">Key to read from.</param>
+        /// <param name="pollingIntervalInMs">How often to poll Redis in ms.</param>
+        /// <param name="messagesPerWorker">The number of messages each functions instance is expected to handle.</param>
+        /// <param name="count">Number of elements to pull from Redis at one time.</param>
+        /// <param name="deleteAfterProcess">Decides if the function will delete the stream entries after processing. Default: false</param>
+        public RedisStreamTriggerAttribute(string connectionStringSetting, string key, int pollingIntervalInMs = 1000, int messagesPerWorker = 100, int count = 10, bool deleteAfterProcess = false)
+        {
+            ConnectionStringSetting = connectionStringSetting;
+            Key = key;
+            PollingIntervalInMs = pollingIntervalInMs;
+            MessagesPerWorker = messagesPerWorker;
+            Count = count;
+            DeleteAfterProcess = deleteAfterProcess;
+        }
+
+        /// <summary>
+        /// Redis connection string setting.
+        /// This setting will be used to resolve the actual connection string from the appsettings.
+        /// </summary>
+        public string ConnectionStringSetting { get; }
+
+        /// <summary>
+        /// Key to read from.
+        /// </summary>
+        public string Key { get; }
+
+        /// <summary>
+        /// How often to poll Redis in milliseconds.
+        /// </summary>
+        public int PollingIntervalInMs { get; }
+
+        /// <summary>
+        /// The number of messages each functions instance is expected to handle.
+        /// Used to determine how many workers the function should scale to.
+        /// For example, if the number of <see cref="MessagesPerWorker">MessagesPerWorker</see> is 10,
+        /// and there are 1500 elements remaining in the list,
+        /// the functions host will attempt to scale up to 150 instances.
+        /// </summary>
+        public int MessagesPerWorker { get; }
+
+        /// <summary>
+        /// Number of elements to pull from Redis at one time.
+        /// </summary>
+        public int Count { get; }
+
+        /// <summary>
+        /// Decides if the function will delete the stream entries after processing.
+        /// </summary>
+        public bool DeleteAfterProcess { get; }
+    }
+}
