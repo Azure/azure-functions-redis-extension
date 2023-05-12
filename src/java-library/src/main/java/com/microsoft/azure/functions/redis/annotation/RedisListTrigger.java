1ec642fdbba5ed53164a14c715e4058766620ab9
Maran Palaniappan <mapalan@microsoft.com>
2023-05-12 10:17:19 -0700

    add java-library and build

---
+++ src/java-library/src/main/java/com/microsoft/azure/functions/redis/annotation/RedisListTrigger.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.functions.redis.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * Java annotation used to bind a parameter to Redis list entry.
+ * </p>
+ *
+ * <p>
+ * Example function that uses a RedisListTrigger binding:
+ * </p>
+ *
+ * <pre>
+ * &#64;FunctionName("RedisListExample")
+ * public void run(
+ *         &#64;RedisListTrigger(connectionStringSetting = "ConnectionString", key = "listkey") String entry,
+ *         final ExecutionContext context) {
+ *     context.getLogger().info("Java Redis List trigger function processed a list entry: " + entry);
+ * }
+ * </pre>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface RedisListTrigger {
+    /**
+     * The variable name used in function.json.
+     * @return The variable name used in function.json.
+     */
+    String name();
+
+    /**
+     * Setting name for Redis connection string.
+     * @return Setting name for Redis connection string.
+     */
+    String connectionStringSetting() default "";
+
+    /**
+     * <p>Defines how Functions runtime should treat the parameter value. Possible values are:</p>
+     * <ul>
+     *     <li>"": get the value as a string, and try to deserialize to actual parameter type like POJO</li>
+     *     <li>string: always get the value as a string</li>
+     *     <li>binary: get the value as a binary data, and try to deserialize to actual parameter type byte[]</li>
+     * </ul>
+     * @return The dataType which will be used by the Functions runtime.
+     */
+    String dataType() default "";
+
+    /**
+     * Key to read from.
+     * @return Key to read from.
+     */
+    String key();
+
+    /**
+     * How often to poll Redis in milliseconds.
+     * @return How often to poll Redis in milliseconds.
+     */
+    int pollingIntervalInMs() default 1000;
+
+    /**
+     * How many messages each functions worker should process.
+     * Used to determine how many workers the function should scale to.
+     * For example, if the messagesPerWorker is 10,
+     * and there are 1500 elements remaining in the list,
+     * the functions host will attempt to scale up to 150 instances.
+     * @return How many messages each functions worker should process.
+     */
+    int messagesPerWorker() default 100;
+
+    /**
+     * Number of elements to pull from Redis at one time.
+     * @return Number of elements to pull from Redis at one time.
+     */
+    int count() default 10;
+
+    /**
+     * Determines whether to pop elements from the beginning using LPOP or to pop elements from the end using RPOP.
+     * @return Whether to pop elements from the beginning or end of the list.
+     */
+    boolean listPopFromBeginning() default true;
+}
---
+++ src/java-library/src/main/java/com/microsoft/azure/functions/redis/annotation/RedisStreamTrigger.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.functions.redis.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * Java annotation used to bind a parameter to Redis stream entry.
+ * </p>
+ *
+ * <p>
+ * Example function that uses a RedisStreamTrigger binding:
+ * </p>
+ *
+ * <pre>
+ * &#64;FunctionName("RedisStreamExample")
+ * public void run(
+ *         &#64;RedisStreamTrigger(connectionStringSetting = "ConnectionString", key = "streamkey") String entry,
+ *         final ExecutionContext context) {
+ *     context.getLogger().info("Java Redis Stream trigger function processed a entry: " + entry);
+ * }
+ * </pre>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface RedisStreamTrigger {
+    /**
+     * The variable name used in function.json.
+     * @return The variable name used in function.json.
+     */
+    String name();
+
+    /**
+     * Setting name for Redis connection string.
+     * @return Setting name for Redis connection string.
+     */
+    String connectionStringSetting() default "";
+
+    /**
+     * <p>Defines how Functions runtime should treat the parameter value. Possible values are:</p>
+     * <ul>
+     *     <li>"": get the value as a string, and try to deserialize to actual parameter type like POJO</li>
+     *     <li>string: always get the value as a string</li>
+     *     <li>binary: get the value as a binary data, and try to deserialize to actual parameter type byte[]</li>
+     * </ul>
+     * @return The dataType which will be used by the Functions runtime.
+     */
+    String dataType() default "";
+
+    /**
+     * Key to read from.
+     * @return Key to read from.
+     */
+    String key();
+
+    /**
+     * How often to poll Redis in milliseconds.
+     * @return How often to poll Redis in milliseconds.
+     */
+    int pollingIntervalInMs() default 1000;
+
+    /**
+     * How many messages each functions worker should process.
+     * Used to determine how many workers the function should scale to.
+     * For example, if the messagesPerWorker is 10,
+     * and there are 1500 elements remaining in the list,
+     * the functions host will attempt to scale up to 150 instances.
+     * @return How many messages each functions worker should process.
+     */
+    int messagesPerWorker() default 100;
+
+    /**
+     * Number of elements to pull from Redis at one time.
+     * @return Number of elements to pull from Redis at one time.
+     */
+    int count() default 10;
+
+    /**
+     * If the listener will delete the stream entries after the function runs.
+     * @return If the listener will delete the stream entries after the function runs.
+     */
+    boolean deleteAfterProcess() default false;
+}
