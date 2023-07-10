51eaa99a216aca2f6938baa8a1e20da2d1509417
Phoebe Owusu <t-powusu@microsoft.com>
2023-07-10 10:43:46 -0700

    Write Around Function

---
+++ samples/dotnet/CosmosToRedis.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
+{
+    internal class CosmosToRedis
+    {
+        private static readonly IDatabase redisDB = ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable("REDIS_CONNECTION")).GetDatabase();
+
+        // Write Around - Cosmos DB to Redis 
+        [FunctionName("CosmosToRedis")]
+        public static void Run(
+            [CosmosDBTrigger(
+                databaseName: "database-id",
+                containerName: "container-id",
+                Connection = "COSMOS_CONNECTION",
+                LeaseContainerName = "leases",
+                CreateLeaseContainerIfNotExists = true)]IReadOnlyList<Data> input, ILogger logger)
+        {
+            foreach (var document in input)
+            {
+                var values = new NameValueEntry[document.values.Count];
+                int i = 0;
+                foreach (KeyValuePair<string, string> entry in document.values)
+                {
+                    values[i++] = new NameValueEntry(entry.Key, entry.Value);
+                }
+
+                redisDB.StreamAddAsync("stream2", values);
+            }
+        }
+    }
+}
+
+public class Data
+{
+    public string id { get; set; }
+    public Dictionary<string, string> values { get; set; }
+}
+
+
