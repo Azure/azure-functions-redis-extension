207f2ca1871f7c70a3f2e262ffd4e287b3b3d3aa
Riley MacDonald <t-rmacdonald@microsoft.com>
2023-07-25 10:59:57 -0700

    Added Docstrings and changed Function trigger parameters so they are resolved from local.settings.json rather than hard coded

--- samples/CosmosDBIntegration/WriteAroundSamples/PubSubSample.cs
+++ samples/CosmosDBIntegration/WriteAroundSamples/PubSubSample.cs
@@ -22,14 +22,11 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
         private static readonly IConnectionMultiplexer s_redisConnection =
             ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable($"ConnectionStrings:{redisConnectionSetting}"));
 
-        private static readonly IDatabaseAsync s_redisDb = s_redisConnection.GetDatabase();
-        private static readonly ISubscriber s_redisPublisher = s_redisConnection.GetSubscriber();
-
 
         /// <summary>
-        /// Triggers when key/value pairs are written directly to Cosmos DB, then asynchronously writes the associated key/value pair to Redis.
+        /// Triggers when key/value pairs are written directly to Cosmos DB then asynchronously writes the associated key/value pair to Redis.
         /// </summary>
-        /// <param name="cosmosData"> A readonly list containing all the new/updated documents in the specified Cosmos DB container.</param>
+        /// <param name="cosmosData"> An IReadonlyList containing all the new/updated documents in the specified Cosmos DB container.</param>
         /// <param name="logger"> An ILogger that is used to write informational log messages.</param>
         /// <returns></returns>
         /// <exception cref="Exception"> Thrown when they key/value pair is already stored in the Redis cache.</exception>
@@ -45,11 +42,12 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
             //if the list is null or empty, return
             if (cosmosData == null || cosmosData.Count <= 0) return;
 
+            IDatabaseAsync s_redisDb = s_redisConnection.GetDatabase();
             //for each item upladed to cosmos, write it to Redis
             foreach (var document in cosmosData)
             {
                 //if the key/value pair is already in Redis, throw an exception
-                if (await s_redisConnection.GetDatabase().StringGetAsync(document.key) == document.value)
+                if (await s_redisDb.StringGetAsync(document.key) == document.value)
                 {
                     throw new Exception($"ERROR: Key: \"{document.key}\", Value: \"{document.value}\" pair is already in Azure Redis Cache.");
                 }
@@ -77,6 +75,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
             //if the list is null or empty, return
             if (cosmosData == null || cosmosData.Count <= 0) return;
 
+            ISubscriber s_redisPublisher = s_redisConnection.GetSubscriber();
             //for each new item upladed to cosmos, publish to Redis
             foreach (var document in cosmosData)
             {
