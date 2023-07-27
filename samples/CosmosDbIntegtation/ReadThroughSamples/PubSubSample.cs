cffb4dea1ee1448263dd8d04a78960b614475d02
Riley MacDonald <t-rmacdonald@microsoft.com>
2023-07-27 10:27:16 -0700

    Removed unnecessary usings

--- samples/CosmosDbIntegtation/ReadThroughSamples/PubSubSample.cs
+++ samples/CosmosDbIntegtation/ReadThroughSamples/PubSubSample.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Linq;
 using Microsoft.Azure.Functions.Worker.Extensions.Redis;
-using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Redis.Samples.Models;
 using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
