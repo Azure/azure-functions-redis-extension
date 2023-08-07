3641b0e1a5af3f221ffae686f2c040b05e14da63
Maran Palaniappan <mapalan@microsoft.com>
2023-08-07 11:08:23 -0700

    move environment variables from test helper to pipeline

--- test/dotnet/Integration/IntegrationTestHelpers.cs
+++ test/dotnet/Integration/IntegrationTestHelpers.cs
@@ -14,9 +14,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
     {
         internal static Process StartFunction(string functionName, int port)
         {
-            Environment.SetEnvironmentVariable("FUNCTIONS_RUNTIME_SCALE_MONITORING_ENABLED", "1");
-            Environment.SetEnvironmentVariable("TARGET_BASED_SCALING_ENABLED", "1");
-
             Process functionsProcess = new Process
             {
                 StartInfo = new ProcessStartInfo
