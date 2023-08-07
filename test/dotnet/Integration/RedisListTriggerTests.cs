89f2da7021095818f0ee566e378c645788133cbf
Maran Palaniappan <mapalan@microsoft.com>
2023-08-07 15:43:32 -0700

    attempt different env variable setting tools

--- test/dotnet/Integration/RedisListTriggerTests.cs
+++ test/dotnet/Integration/RedisListTriggerTests.cs
@@ -110,36 +110,37 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
         [Fact]
         public async void ListTrigger_TargetBasedScaling_WorksCorrectly()
         {
-            throw new Exception(Environment.GetEnvironmentVariable("FUNCTIONS_RUNTIME_SCALE_MONITORING_ENABLED"));
-            //if (monitoringEnabled != "1")
-            //{
-            //    throw new Exception("FUNCTIONS_RUNTIME_SCALE_MONITORING_ENABLED not set to 1, cannot attempt to check status");
-            //}
+            string functionName = nameof(RedisListTriggerTestFunctions.ListTrigger_RedisValue_LongPollingInterval);
+            int port = 7071;
+            int elements = 10000;
+            RedisValue[] valuesArray = Enumerable.Range(0, elements).Select(x => new RedisValue(x.ToString())).ToArray();
 
-            //string functionName = nameof(RedisListTriggerTestFunctions.ListTrigger_RedisValue_LongPollingInterval);
-            //int port = 7071;
-            //int elements = 10000;
-            //RedisValue[] valuesArray = Enumerable.Range(0, elements).Select(x => new RedisValue(x.ToString())).ToArray();
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisListTriggerTestFunctions.localhostSetting)))
+            {
+                await multiplexer.GetDatabase().ListLeftPushAsync(functionName, valuesArray);
+                await multiplexer.CloseAsync();
+            };
 
-            //using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisListTriggerTestFunctions.localhostSetting)))
-            //{
-            //    await multiplexer.GetDatabase().ListLeftPushAsync(functionName, valuesArray);
-            //    await multiplexer.CloseAsync();
-            //};
+            IntegrationTestHelpers.ScaleStatus status = new IntegrationTestHelpers.ScaleStatus { vote = 0, targetWorkerCount = 0 };
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName, port))
+            using (HttpClient client = new HttpClient())
+            using (StringContent jsonContent = new StringContent("{}", Encoding.UTF8, "application/json"))
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(new { name = functionName, arguments = new string[] { "1" } }));
+                try
+                {
+                    HttpResponseMessage response = await client.PostAsync($"http://127.0.0.1:{port}/admin/host/scale/status", jsonContent);
+                    status = JsonConvert.DeserializeObject<IntegrationTestHelpers.ScaleStatus>(await response.Content.ReadAsStringAsync());
+                }
+                catch (HttpRequestException ex)
+                {
+                    //Environment variable FUNCTIONS_RUNTIME_SCALE_MONITORING_ENABLED not set to 1, so test will not work.
+                }
+                functionsProcess.Kill();
+            };
 
-            //IntegrationTestHelpers.ScaleStatus status;
-            //using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName, port))
-            //using (HttpClient client = new HttpClient())
-            //using (StringContent jsonContent = new StringContent("{}", Encoding.UTF8, "application/json"))
-            //{
-            //    StringContent content = new StringContent(JsonConvert.SerializeObject(new { name = functionName, arguments = new string[] { "1" } }));
-            //    HttpResponseMessage response = await client.PostAsync($"http://127.0.0.1:{port}/admin/host/scale/status", jsonContent);
-            //    status = JsonConvert.DeserializeObject<IntegrationTestHelpers.ScaleStatus>(await response.Content.ReadAsStringAsync());
-            //    functionsProcess.Kill();
-            //};
-
-            //Assert.Equal(1, status.vote);
-            //Assert.Equal(elements - 1, status.targetWorkerCount);
+            Assert.Equal(1, status.vote);
+            Assert.Equal(elements - 1, status.targetWorkerCount);
         }
     }
 }
