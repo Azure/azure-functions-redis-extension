1558a01be0d2b0e071a556fb6570953d7803baaf
phoebeowusu <137229333+phoebeowusu@users.noreply.github.com>
2023-08-09 09:56:48 -0700

    Sample Function templates for Redis to CosmosDB Synchronization using the Redis Stream Trigger (#99)
    Co-authored-by: Phoebe Owusu <t-powusu@microsoft.com>

--- samples/CosmosDBIntegration/Models/RedisData.cs
+++ samples/CosmosDBIntegration/Models/RedisData.cs
@@ -6,26 +6,64 @@ using System.Linq;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples.Models
 {
-    public class CosmosDBData
+    public class StreamData
     {
         public string id { get; set; }
         public Dictionary<string, string> values { get; set; }
 
         // Helper method to format stream message
-        public static CosmosDBData Format(StreamEntry entry, ILogger logger)
+        public static StreamData Format(StreamEntry entry, ILogger logger)
         {
             logger.LogInformation("ID: {val}", entry.Id.ToString());
 
             // Map each key value pair
-            Dictionary<string, string> dict = StreamEntryToDictionary(entry);
+            Dictionary<string, string> dict = RedisUtilities.StreamEntryToDictionary(entry);
 
-            CosmosDBData sampleItem = new CosmosDBData { id = entry.Id, values = dict };
+            StreamData sampleItem = new StreamData { id = entry.Id, values = dict };
             return sampleItem;
         }
+    }
 
-        internal static Dictionary<string, string> StreamEntryToDictionary(StreamEntry entry)
+    public class StreamDataSingleDocument
+    {
+        public string id { get; set; }
+        public int maxlen { get; set; }
+        public Dictionary<string, Dictionary<string, string>> messages { get; set; }
+
+        public static StreamDataSingleDocument CreateNewEntry(StreamEntry entry, string streamName, ILogger logger)
         {
-            return entry.Values.ToDictionary((NameValueEntry value) => value.Name.ToString(), (NameValueEntry value) => value.Value.ToString());
+            logger.LogInformation("Creating a new document for {val}. Inserting ID: {val} as the first entry", streamName, entry.Id.ToString());
+
+            // Map each key value pair
+            Dictionary<string, string> dict = RedisUtilities.StreamEntryToDictionary(entry);
+
+            // Create a new list of messages
+            var list = new Dictionary<string, Dictionary<string, string>>();
+            list.Add(entry.Id.ToString(), dict);
+
+            StreamDataSingleDocument data = new StreamDataSingleDocument { id = streamName, maxlen = 1000, messages = list };
+            return data;
+        }
+
+        public static StreamDataSingleDocument UpdateExistingEntry(StreamDataSingleDocument results, StreamEntry entry, ILogger logger)
+        {
+            logger.LogInformation("Adding to {val} document. Inserting ID: {val} ", results.id, entry.Id.ToString());
+
+            // Map each key value pair
+            Dictionary<string, string> dict = RedisUtilities.StreamEntryToDictionary(entry);
+
+            // Update list of messages
+            var list = results.messages;
+            list.Add(entry.Id.ToString(), dict);
+
+            if (list.Count > results.maxlen)
+            {
+                string minKey = list.Keys.Min();
+                list.Remove(minKey);
+            }
+
+            StreamDataSingleDocument data = new StreamDataSingleDocument { id = results.id, maxlen = results.maxlen, messages = list };
+            return data;
         }
     }
 
--- samples/CosmosDBIntegration/WriteAroundSamples/StreamSample.cs
+++ samples/CosmosDBIntegration/WriteAroundSamples/StreamSample.cs
@@ -10,15 +10,15 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
     internal class StreamSample
     {
         // Redis database and stream stored in local.settings.json
-        public const string redisConnectionSetting = "redisConnectionString";
-        private static readonly Lazy<IDatabaseAsync> redisDB = new Lazy<IDatabaseAsync>(() =>
-           ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable(redisConnectionSetting)).GetDatabase());
-        public const string stream = "streamTest";
+        public const string RedisConnectionSetting = "RedisConnectionString";
+        private static readonly Lazy<IDatabaseAsync> _redisDB = new Lazy<IDatabaseAsync>(() =>
+           ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable(RedisConnectionSetting)).GetDatabase());
+        public static string StreamName = Environment.GetEnvironmentVariable("StreamTest");
 
         // CosmosDB connection string, database name and container name stored in local.settings.json
-        public const string cosmosDbConnectionSetting = "cosmosDbConnectionString";
-        public const string databaseSetting = "%cosmosDbDatabaseId%";
-        public const string containerSetting = "%cosmosDbContainerId%";
+        public const string CosmosDbConnectionSetting = "CosmosDbConnectionString";
+        public const string DatabaseSetting = "%CosmosDbDatabaseId%";
+        public const string ContainerSetting = "%CosmosDbContainerId%";
 
         /// <summary>
         /// Write Around: Write from Cosmos DB to Redis whenever a change occurs in one of the CosmosDB documents
@@ -28,27 +28,71 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
         [FunctionName(nameof(WriteAroundForStreamAsync))]
         public static async Task WriteAroundForStreamAsync(
             [CosmosDBTrigger(
-                databaseName: databaseSetting,
-                containerName: containerSetting,
-                Connection = cosmosDbConnectionSetting,
+                databaseName: DatabaseSetting,
+                containerName: ContainerSetting,
+                Connection = CosmosDbConnectionSetting,
                 LeaseContainerName = "leases",
-                CreateLeaseContainerIfNotExists = true)]IReadOnlyList<CosmosDBData> input, ILogger logger)
+                CreateLeaseContainerIfNotExists = true)]IReadOnlyList<StreamData> input, ILogger logger)
         {
-            if (input != null)
+            if (input == null) return;
+
+            // Iterate through each changed document
+            foreach (var document in input)
             {
-                foreach (var document in input)
+                logger.LogInformation("{messageID} changed and sent to {stream} ", document.id, StreamName);
+
+                var values = new NameValueEntry[document.values.Count];
+                int i = 0;
+
+                // Format the key/value pairs
+                foreach (KeyValuePair<string, string> entry in document.values)
                 {
-                    logger.LogInformation(document.id + " changed");
-                    var values = new NameValueEntry[document.values.Count];
+                    values[i++] = new NameValueEntry(entry.Key, entry.Value);
+                }
+
+                // Upload value to Redis Stream
+                await _redisDB.Value.StreamAddAsync(StreamName, values);
+            }     
+        }
+       
+        public const string ContainerSettingSingleDocument = "%CosmosDbContainerIdSingleDocument%";
+        public static string StreamNameSingleDocument = Environment.GetEnvironmentVariable("StreamTestSingleDocument");
+
+        /// <summary>
+        /// Write Around (Single Document): Write from Cosmos DB to Redis whenever a change occurs in one of the CosmosDB documents
+        /// </summary>
+        /// <param name="input"> List of changed documents in CosmosDB </param>
+        /// <param name="logger"> ILogger used to write key information </param>
+        [FunctionName(nameof(CosmosToRedisForStreamSingleDocumentAsync))]
+        public static async Task CosmosToRedisForStreamSingleDocumentAsync(
+            [CosmosDBTrigger(
+                databaseName: DatabaseSetting,
+                containerName: ContainerSettingSingleDocument,
+                Connection = CosmosDbConnectionSetting,
+                LeaseContainerName = "leases",
+                CreateLeaseContainerIfNotExists = true)]IReadOnlyList<StreamDataSingleDocument> input, ILogger logger)
+        {
+            if (input == null) return;
+
+            // Iterate through each changed document
+            foreach (var document in input)
+            {
+                logger.LogInformation("{stream1} changed and sent to {stream2} ", document.id, StreamNameSingleDocument);
+
+                // Go through each message and format the key/value pairs
+                foreach (var message in document.messages)
+                {
+                    var values = new NameValueEntry[message.Value.Count];
                     int i = 0;
-                    foreach (KeyValuePair<string, string> entry in document.values)
+                    foreach (var entry in message.Value)
                     {
                         values[i++] = new NameValueEntry(entry.Key, entry.Value);
                     }
 
-                    await redisDB.Value.StreamAddAsync(stream, values);
+                    // Upload value to Redis Stream
+                    await _redisDB.Value.StreamAddAsync(StreamNameSingleDocument, values, messageId: message.Key, maxLength: document.maxlen);
                 }
-            }
-        }
-     }
+            }  
+        }  
+    }
 }
