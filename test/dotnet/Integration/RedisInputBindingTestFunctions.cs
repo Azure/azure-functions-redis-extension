11cce5003c2f968667f450a43c21b072f6f33d2e
Maran Palaniappan <mapalan@microsoft.com>
2023-09-11 14:35:19 -0700

    add custom type to integration tests:

--- test/dotnet/Integration/RedisInputBindingTestFunctions.cs
+++ test/dotnet/Integration/RedisInputBindingTestFunctions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
 {
@@ -12,7 +13,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
             [Redis(localhostSetting, $"GET {nameof(GetTester)}")] string value,
             ILogger logger)
         {
-            logger.LogInformation($"Value of key '{nameof(GetTester)}' is currently '{value}'");
+            logger.LogInformation($"Value of key '{nameof(GetTester)}' is currently a type {value.GetType()}: '{value}'");
         }
 
         [FunctionName(nameof(HgetTester))]
@@ -22,6 +23,15 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
             ILogger logger)
         {
             logger.LogInformation($"Value of field 'field' in hash '{nameof(HgetTester)}' is currently '{value}'");
+        }
+
+        [FunctionName(nameof(GetTester_Custom))]
+        public static void GetTester_Custom(
+            [RedisPubSubTrigger(localhostSetting,nameof(GetTester_Custom))] string message,
+            [Redis(localhostSetting, $"GET {nameof(GetTester_Custom)}")] CustomType value,
+            ILogger logger)
+        {
+            logger.LogInformation($"Value of key '{nameof(GetTester_Custom)}' is currently a type {value.GetType()}: '{JsonConvert.SerializeObject(value)}'");
         }
     }
 }
--- test/dotnet/Integration/RedisInputBindingTests.cs
+++ test/dotnet/Integration/RedisInputBindingTests.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
             string value = "value";
             ConcurrentDictionary<string, int> counts = new ConcurrentDictionary<string, int>();
             counts.TryAdd($"Executed '{functionName}' (Succeeded", 1);
-            counts.TryAdd($"Value of key '{functionName}' is currently '{value}'", 1);
+            counts.TryAdd($"Value of key '{functionName}' is currently a type {value.GetType()}: '{value}'", 1);
 
             using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisListTriggerTestFunctions.localhostSetting)))
             {
@@ -68,6 +68,32 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
                 var incorrect = counts.Where(pair => pair.Value != 0);
                 Assert.False(incorrect.Any(), JsonConvert.SerializeObject(incorrect));
             }
+        }
+
+        [Fact]
+        public async void GetCustom_SuccessfullyConvertsType()
+        {
+            string functionName = nameof(RedisInputBindingTestFunctions.GetTester_Custom);
+            CustomType value = new CustomType { Field = "a", Name = "b", Random = "c" };
+            ConcurrentDictionary<string, int> counts = new ConcurrentDictionary<string, int>();
+            counts.TryAdd($"Executed '{functionName}' (Succeeded", 1);
+            counts.TryAdd($"Value of key '{functionName}' is currently a type {value.GetType()}: '{JsonConvert.SerializeObject(value)}'", 1);
+
+            using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, RedisListTriggerTestFunctions.localhostSetting)))
+            using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName, 7071))
+            {
+                await multiplexer.GetDatabase().KeyDeleteAsync(functionName);
+                functionsProcess.OutputDataReceived += IntegrationTestHelpers.CounterHandlerCreator(counts);
+                await multiplexer.GetDatabase().StringSetAsync(functionName, JsonConvert.SerializeObject(value));
+                await multiplexer.GetSubscriber().PublishAsync(functionName, "start");
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                await multiplexer.CloseAsync();
+                functionsProcess.Kill();
+            };
+            var incorrect = counts.Where(pair => pair.Value != 0);
+            Assert.False(incorrect.Any(), JsonConvert.SerializeObject(incorrect));
         }
     }
 }
