71ef0f45d8f2dc41af4674e73488191dc31c1e28
Maran Palaniappan (from Dev Box) <mapalan@microsoft.com>
2023-12-05 13:32:53 -0800

    update samples and common classes

--- samples/dotnet-isolated/Common.cs
+++ samples/dotnet-isolated/Common.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.Redis.Samples
         public class CustomType
         {
             public string Name { get; set; }
-            public long Id { get; set; }
+            public string Id { get; set; }
             public string Field { get; set; }
         }
     }
