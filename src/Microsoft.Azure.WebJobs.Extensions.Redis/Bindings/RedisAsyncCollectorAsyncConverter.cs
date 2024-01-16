9a6a248811e20ad731bd1f8276f5e591f3402441
Maran Palaniappan (from Dev Box) <mapalan@microsoft.com>
2024-01-16 10:59:33 -0800

    change creation of RedisAsyncCollector to use AsyncConverter

---
+++ src/Microsoft.Azure.WebJobs.Extensions.Redis/Bindings/RedisAsyncCollectorAsyncConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Redis
+{
+    /// <summary>
+    /// Used to create the RedisAsyncCollector.
+    /// </summary>
+    internal class RedisAsyncCollectorAsyncConverter : IAsyncConverter<RedisAttribute, IAsyncCollector<string>>
+    {
+        private IConfiguration configuration;
+        private ILogger logger;
+
+        public RedisAsyncCollectorAsyncConverter(IConfiguration configuration, ILogger logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        public async Task<IAsyncCollector<string>> ConvertAsync(RedisAttribute input, CancellationToken cancellationToken)
+        {
+            IConnectionMultiplexer multiplexer = RedisExtensionConfigProvider.GetOrCreateConnectionMultiplexer(configuration, input.ConnectionStringSetting);
+            return new RedisAsyncCollector(multiplexer, input.Command, logger);
+        }
+    }
+}
