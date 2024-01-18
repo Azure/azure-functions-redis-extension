62e472fbe06e3e1611e110cf68ff4d8cc2edf570
Maran Palaniappan (from Dev Box) <mapalan@microsoft.com>
2024-01-18 09:17:54 -0800

    fix test

--- test/dotnet/Integration/RedisOutputBindingTests.cs
+++ test/dotnet/Integration/RedisOutputBindingTests.cs
@@ -59,7 +59,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
             }
 
             long length = 1;
-            //using (Process redisProcess = IntegrationTestHelpers.StartRedis(IntegrationTestHelpers.Redis60))
+            using (Process redisProcess = IntegrationTestHelpers.StartRedis(IntegrationTestHelpers.Redis60))
             using (ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(RedisUtilities.ResolveConnectionString(IntegrationTestHelpers.localsettings, IntegrationTestHelpers.ConnectionStringSetting)))
             {
                 using (Process functionsProcess = IntegrationTestHelpers.StartFunction(functionName, 7071))
@@ -73,7 +73,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
                     length = await multiplexer.GetDatabase().StreamLengthAsync(functionName);
                     await multiplexer.CloseAsync();
                     functionsProcess.Kill();
-                    //IntegrationTestHelpers.StopRedis(redisProcess);
+                    IntegrationTestHelpers.StopRedis(redisProcess);
                 };
                 var incorrect = counts.Where(pair => pair.Value != 0);
                 Assert.False(incorrect.Any(), JsonConvert.SerializeObject(incorrect));
