005951db575749b7433cdee5bf96bc0b3125949b
Maran Palaniappan (from Dev Box) <mapalan@microsoft.com>
2024-02-06 17:57:23 -0800

    add MI test for each

---
+++ test/dotnet/Integration/RedisStreamTriggerTestFunctions/StreamTrigger_String_ManagedIdentity.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Redis.Tests.Integration
+{
+    public static class StreamTrigger_String_ManagedIdentity
+    {
+        [FunctionName(nameof(StreamTrigger_String_ManagedIdentity))]
+        public static void Run(
+            [RedisStreamTrigger(IntegrationTestHelpers.ManagedIdentitySetting, nameof(StreamTrigger_String_ManagedIdentity), IntegrationTestHelpers.PollingIntervalShort)] string values,
+            ILogger logger)
+        {
+            logger.LogInformation(IntegrationTestHelpers.GetLogValue(values));
+        }
+    }
+}
